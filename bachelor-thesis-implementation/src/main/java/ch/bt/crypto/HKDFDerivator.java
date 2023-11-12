179de03dca733e4235e57e659008f136a6ab46eb
Janet Greutmann <janetgr@student.ethz.ch>
2023-11-12 18:41:54 +0100

    make key derivator generic

--- bachelor-thesis-implementation/src/main/java/ch/bt/crypto/HKDFDerivator.java
+++ bachelor-thesis-implementation/src/main/java/ch/bt/crypto/HKDFDerivator.java
@@ -2,18 +2,19 @@ package ch.bt.crypto;
 
 import org.bouncycastle.crypto.Digest;
 import org.bouncycastle.crypto.digests.SHA256Digest;
+import org.bouncycastle.crypto.digests.SHA512Digest;
 import org.bouncycastle.crypto.generators.HKDFBytesGenerator;
 import org.bouncycastle.crypto.params.HKDFParameters;
 
 public class HKDFDerivator implements KeyDerivator {
     private final HKDFBytesGenerator hkdfBytesGenerator;
-    private static final Digest DEFAULT_HASH = new SHA256Digest();
 
-    public HKDFDerivator(){
-        this.hkdfBytesGenerator = new HKDFBytesGenerator(DEFAULT_HASH);
-    }
-
-    public HKDFDerivator(Digest hash){
+    public HKDFDerivator(final int securityParameter){
+        final var hash = switch(securityParameter){
+            case 256 -> new SHA256Digest();
+            case 512 -> new SHA512Digest();
+            default -> throw new IllegalArgumentException("security parameter doesn't match hash");
+        };
         this.hkdfBytesGenerator = new HKDFBytesGenerator(hash);
     }
 
