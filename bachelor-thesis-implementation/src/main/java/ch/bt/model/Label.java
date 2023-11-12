78d9e67d45707178961b852106ff42bd0f2705e2
Janet Greutmann <janetgr@student.ethz.ch>
2023-11-12 18:41:42 +0100

    make label and value comparable

--- bachelor-thesis-implementation/src/main/java/ch/bt/model/Label.java
+++ bachelor-thesis-implementation/src/main/java/ch/bt/model/Label.java
@@ -1,13 +1,32 @@
 package ch.bt.model;
 
-public class Label {
+import java.util.Arrays;
+
+public class Label implements Comparable<Label> {
     private final byte[] label;
 
-    public Label(final byte[] label){
+    public Label(final byte[] label) {
         this.label = label;
     }
 
-    public byte[] getLabel(){
+    public byte[] getLabel() {
         return this.label;
+    }
+
+    @Override
+    public int compareTo(Label other) {
+        return Arrays.compare(label, other.label);
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (!(o instanceof Label)) {
+            return false;
+        }
+        final var other = (Label) o;
+        return Arrays.equals(label, other.label);
     }
 }
--- bachelor-thesis-implementation/src/main/java/ch/bt/model/Value.java
+++ bachelor-thesis-implementation/src/main/java/ch/bt/model/Value.java
@@ -1,13 +1,33 @@
 package ch.bt.model;
 
-public class Value {
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Value implements Comparable<Value> {
     private final byte[] value;
 
-    public Value(final byte[] value){
+    public Value(final byte[] value) {
         this.value = value;
     }
 
-    public byte[] getValue(){
+    public byte[] getValue() {
         return this.value;
+    }
+
+    @Override
+    public int compareTo(Value other) {
+        return Arrays.compare(value, other.value);
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (!(o instanceof Value)) {
+            return false;
+        }
+        final var other = (Value) o;
+        return Arrays.equals(value, other.value);
     }
 }
