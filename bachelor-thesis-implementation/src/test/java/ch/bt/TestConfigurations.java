fa355c69565c62cf0c5c8c3e4baf7e8369ab39bd
Janet Greutmann <janetgr@student.ethz.ch>
2023-11-21 19:56:59 +0100

    add testcontainer

--- bachelor-thesis-implementation/src/test/java/ch/bt/TestConfigurations.java
+++ bachelor-thesis-implementation/src/test/java/ch/bt/TestConfigurations.java
@@ -3,7 +3,11 @@ package ch.bt;
 import org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.utility.MountableFile;
 
+import java.io.IOException;
 import java.security.Security;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -12,23 +16,34 @@ import java.sql.SQLException;
 /**
  * <a
  * href="https://stackoverflow.com/questions/43282798/in-junit-5-how-to-run-code-before-all-tests">...</a>
+ * <a href="https://www.baeldung.com/docker-test-containers">...</a> <a
+ * href="https://github.com/mikemybytes/squash-db-migrations">...</a>
  */
 public class TestConfigurations implements BeforeAllCallback {
     private static boolean started = false;
     public static Connection connection;
 
     @Override
-    public void beforeAll(ExtensionContext extensionContext) {
+    public void beforeAll(ExtensionContext extensionContext)
+            throws SQLException, IOException, InterruptedException {
         if (!started) {
             started = true;
             Security.addProvider(new BouncyCastleFipsProvider());
-            try {
-                connection =
-                        DriverManager.getConnection(
-                                "jdbc:postgresql://localhost:5432/bt", "bt", "bt");
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
+
+            PostgreSQLContainer<?> postgreSQLContainer =
+                    new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"))
+                            .withReuse(true)
+                            /*.withCopyToContainer(
+                                    MountableFile.forHostPath(
+                                            "bachelor-thesis-implementation/src/test/resources/data/",
+                                            511),
+                                    "/home/data/")*/
+                            .withInitScript("init.sql");
+            postgreSQLContainer.start();
+            String jdbcUrl = postgreSQLContainer.getJdbcUrl();
+            String username = postgreSQLContainer.getUsername();
+            String password = postgreSQLContainer.getPassword();
+            connection = DriverManager.getConnection(jdbcUrl, username, password);
             TestUtils.init();
             extensionContext
                     .getRoot()
