2021a7b9a2e781d8d8b9605e2233967649393f55
Janet Greutmann <janetgr@student.ethz.ch>
2023-12-13 17:22:48 +0100

    add benchmark for parallel range scheme

--- bachelor-thesis-implementation/src/test/java/ch/bt/benchmark/BenchmarkRunner.java
+++ bachelor-thesis-implementation/src/test/java/ch/bt/benchmark/BenchmarkRunner.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 
 public class BenchmarkRunner {
 
-    public static final int ITERATIONS = 10;
+    public static final int ITERATIONS = 2;
 
     public static final int WARM_UPS = 1;
 
@@ -21,10 +21,14 @@ public class BenchmarkRunner {
     private static final String FOLDER = "src/test/resources/benchmark";
 
     private static Options createOptions(
-            final String folder, final String method, final TimeUnit timeUnit) {
+            final String folder,
+            final String method,
+            final TimeUnit timeUnit,
+            final boolean parallel) {
         final String path = String.valueOf(folder.charAt(0)).toLowerCase() + folder.substring(1);
         final String logs = String.join("", "benchmark-logs-", method, ".txt");
         final String results = String.join("", "benchmark-results-", method, ".csv");
+        final String subFolder = parallel ? "parallel" : "sequential";
         return new OptionsBuilder()
                 .jvmArgsPrepend("-server")
                 .include(folder + method + "[0-9]?")
@@ -37,26 +41,73 @@ public class BenchmarkRunner {
                 .threads(THREADS)
                 .measurementIterations(ITERATIONS)
                 .resultFormat(ResultFormatType.CSV)
-                .result(String.join("/", FOLDER, path, results))
+                .result(String.join("/", FOLDER, path, subFolder, results))
                 .verbosity(VerboseMode.EXTRA)
-                .output(String.join("/", FOLDER, path, logs))
+                .output(String.join("/", FOLDER, path, subFolder, logs))
                 .build();
     }
 
     public static void main(String[] args) throws RunnerException {
-        new Runner(createOptions("Baseline", "BuildIndex", TimeUnit.MILLISECONDS)).run();
-        new Runner(createOptions("Baseline", "Trapdoor", TimeUnit.NANOSECONDS)).run();
-        new Runner(createOptions("Baseline", "Search", TimeUnit.NANOSECONDS)).run();
-        new Runner(createOptions("VolumeHiding", "BuildIndex", TimeUnit.MILLISECONDS)).run();
-        new Runner(createOptions("VolumeHiding", "Trapdoor", TimeUnit.NANOSECONDS)).run();
-        new Runner(createOptions("VolumeHiding", "Search", TimeUnit.NANOSECONDS)).run();
-        new Runner(createOptions("VolumeHidingOpt", "BuildIndex", TimeUnit.MILLISECONDS)).run();
-        new Runner(createOptions("VolumeHidingOpt", "Trapdoor", TimeUnit.NANOSECONDS)).run();
-        new Runner(createOptions("VolumeHidingOpt", "Search", TimeUnit.NANOSECONDS)).run();
-        new Runner(createOptions("DpVolumeHiding", "BuildIndex", TimeUnit.MILLISECONDS)).run();
-        new Runner(createOptions("DpVolumeHiding", "Trapdoor", TimeUnit.NANOSECONDS)).run();
-        new Runner(createOptions("DpVolumeHiding", "Search", TimeUnit.NANOSECONDS)).run();
-        new Runner(createOptions("DpVolumeHiding", "Trapdoor2", TimeUnit.NANOSECONDS)).run();
-        new Runner(createOptions("DpVolumeHiding", "Search2", TimeUnit.NANOSECONDS)).run();
+
+        // SEQUENTIAL RANGE SCHEMES
+        // -------------------------------------------------------------------------------------------------
+
+        // Baseline
+        new Runner(createOptions("Baseline", "BuildIndex", TimeUnit.MILLISECONDS, false)).run();
+        new Runner(createOptions("Baseline", "Trapdoor", TimeUnit.NANOSECONDS, false)).run();
+        new Runner(createOptions("Baseline", "Search", TimeUnit.MILLISECONDS, false)).run();
+
+        // Volume Hiding
+        new Runner(createOptions("VolumeHiding", "BuildIndex", TimeUnit.MILLISECONDS, false)).run();
+        new Runner(createOptions("VolumeHiding", "Trapdoor", TimeUnit.NANOSECONDS, false)).run();
+        new Runner(createOptions("VolumeHiding", "Search", TimeUnit.MILLISECONDS, false)).run();
+
+        // Volume Hiding Optimised
+        new Runner(createOptions("VolumeHidingOpt", "BuildIndex", TimeUnit.MILLISECONDS, false))
+                .run();
+        new Runner(createOptions("VolumeHidingOpt", "Trapdoor", TimeUnit.NANOSECONDS, false)).run();
+        new Runner(createOptions("VolumeHidingOpt", "Search", TimeUnit.MILLISECONDS, false)).run();
+
+        // Differentially Private Volume Hiding
+        new Runner(createOptions("DpVolumeHiding", "BuildIndex", TimeUnit.MILLISECONDS, false))
+                .run();
+        new Runner(createOptions("DpVolumeHiding", "Trapdoor", TimeUnit.NANOSECONDS, false)).run();
+        new Runner(createOptions("DpVolumeHiding", "Search", TimeUnit.MILLISECONDS, false)).run();
+        new Runner(createOptions("DpVolumeHiding", "Trapdoor2", TimeUnit.NANOSECONDS, false)).run();
+        new Runner(createOptions("DpVolumeHiding", "Search2", TimeUnit.MILLISECONDS, false)).run();
+
+        // -------------------------------------------------------------------------------------------------
+        // PARALLEL RANGE SCHEMES
+        // -------------------------------------------------------------------------------------------------
+
+        // Baseline
+        new Runner(createOptions("Baseline", "BuildIndexPar", TimeUnit.MILLISECONDS, true)).run();
+        new Runner(createOptions("Baseline", "TrapdoorPar", TimeUnit.NANOSECONDS, true)).run();
+        new Runner(createOptions("Baseline", "SearchPar", TimeUnit.MILLISECONDS, true)).run();
+
+        // Volume Hiding
+        new Runner(createOptions("VolumeHiding", "BuildIndexPar", TimeUnit.MILLISECONDS, true))
+                .run();
+        new Runner(createOptions("VolumeHiding", "TrapdoorPar", TimeUnit.NANOSECONDS, true)).run();
+        new Runner(createOptions("VolumeHiding", "SearchPar", TimeUnit.MILLISECONDS, true)).run();
+
+        // Volume Hiding Optimised
+        new Runner(createOptions("VolumeHidingOpt", "BuildIndexPar", TimeUnit.MILLISECONDS, true))
+                .run();
+        new Runner(createOptions("VolumeHidingOpt", "TrapdoorPar", TimeUnit.NANOSECONDS, true))
+                .run();
+        new Runner(createOptions("VolumeHidingOpt", "SearchPar", TimeUnit.MILLISECONDS, true))
+                .run();
+
+        // Differentially Private Volume Hiding
+        new Runner(createOptions("DpVolumeHiding", "BuildIndexPar", TimeUnit.MILLISECONDS, true))
+                .run();
+        new Runner(createOptions("DpVolumeHiding", "TrapdoorPar", TimeUnit.NANOSECONDS, true))
+                .run();
+        new Runner(createOptions("DpVolumeHiding", "SearchPar", TimeUnit.MILLISECONDS, true)).run();
+        new Runner(createOptions("DpVolumeHiding", "TrapdoorPar2", TimeUnit.NANOSECONDS, true))
+                .run();
+        new Runner(createOptions("DpVolumeHiding", "SearchPar2", TimeUnit.MILLISECONDS, true))
+                .run();
     }
 }
---
+++ bachelor-thesis-implementation/src/test/java/ch/bt/benchmark/volumeHidingOpt/sequential/VolumeHidingOptSearch.java
@@ -0,0 +1,159 @@
+package ch.bt.benchmark.volumeHidingOpt.sequential;
+
+import ch.bt.TestUtils;
+import ch.bt.benchmark.BenchmarkUtils;
+import ch.bt.emm.volumeHiding.VolumeHidingEMMOptimised;
+import ch.bt.genericRs.RangeBRCScheme;
+import ch.bt.model.encryptedindex.EncryptedIndex;
+import ch.bt.model.multimap.Ciphertext;
+import ch.bt.model.multimap.Label;
+import ch.bt.model.multimap.Plaintext;
+import ch.bt.model.rc.CustomRange;
+import ch.bt.model.rc.Vertex;
+import ch.bt.model.searchtoken.SearchToken;
+import ch.bt.rc.BestRangeCover;
+import ch.bt.rc.RangeCoverUtils;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider;
+import org.jetbrains.annotations.NotNull;
+import org.openjdk.jmh.annotations.*;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.security.GeneralSecurityException;
+import java.security.Security;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class VolumeHidingOptSearch {
+
+    @State(Scope.Benchmark)
+    public static class Constants {
+        final String folder = "src/test/resources/benchmark/volumeHidingOpt/sequential";
+        final String method = "search";
+    }
+
+    @State(Scope.Benchmark)
+    public static class ResultPrinter {
+        BufferedWriter fileWriter;
+        CSVFormat csvFormat;
+        CSVPrinter printer;
+
+        public void printToCsv(
+                final String col1,
+                final int col2,
+                final int col3,
+                final String col4,
+                final int col5,
+                final String col6,
+                final int col7,
+                @NotNull Constants constants)
+                throws IOException, SQLException, GeneralSecurityException {
+            if (printer == null) {
+                init(constants);
+            }
+            printer.printRecord(col1, col2, col3, col4, col5, col6, col7);
+        }
+
+        @Setup(Level.Trial)
+        public void init(@NotNull Constants constants)
+                throws GeneralSecurityException, IOException, SQLException {
+            final String file =
+                    String.join(".", String.join("-", "results", constants.method), "csv");
+            fileWriter =
+                    Files.newBufferedWriter(
+                            Paths.get(String.join("/", constants.folder, file)),
+                            StandardOpenOption.APPEND,
+                            StandardOpenOption.CREATE);
+            csvFormat = CSVFormat.DEFAULT.builder().build();
+            printer = new CSVPrinter(fileWriter, csvFormat);
+        }
+
+        @TearDown(Level.Trial)
+        public void tearDown() throws IOException {
+            printer.close();
+        }
+    }
+
+    @State(Scope.Benchmark)
+    public static class Parameters {
+        Map<Label, Set<Plaintext>> multimap;
+        RangeBRCScheme rangeBRCScheme;
+        Vertex root;
+        EncryptedIndex encryptedIndex;
+
+        @Setup(Level.Trial)
+        public void init() throws GeneralSecurityException, IOException, SQLException {
+            Security.addProvider(new BouncyCastleFipsProvider());
+
+            PostgreSQLContainer<?> postgreSQLContainer =
+                    new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"))
+                            .withReuse(true)
+                            .withInitScript("init.sql");
+            postgreSQLContainer.start();
+            String jdbcUrl = postgreSQLContainer.getJdbcUrl();
+            String username = postgreSQLContainer.getUsername();
+            String password = postgreSQLContainer.getPassword();
+            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+            BenchmarkUtils.addData(connection);
+
+            multimap = TestUtils.getDataFromDB(connection);
+            root = RangeCoverUtils.getRoot(multimap);
+
+            final int securityParameter = 256;
+
+            final var emm = new VolumeHidingEMMOptimised(securityParameter, TestUtils.ALPHA);
+            rangeBRCScheme = new RangeBRCScheme(securityParameter, emm, new BestRangeCover(), root);
+            encryptedIndex = rangeBRCScheme.buildIndex(multimap);
+        }
+    }
+
+    @State(Scope.Thread)
+    public static class RangeSchemeState {
+        List<SearchToken> searchToken;
+        CustomRange range;
+
+        @Setup(Level.Trial)
+        public void sampleRange(
+                @NotNull ResultPrinter printer,
+                @NotNull Constants constants,
+                @NotNull Parameters parameters)
+                throws IOException, SQLException, GeneralSecurityException {
+            final var rootRange = parameters.root.range();
+            final int max = rootRange.getMaximum();
+            int size = (int) (Math.random() * rootRange.size());
+            int from = (int) (Math.random() * max) + rootRange.getMinimum();
+            range = new CustomRange(from, Math.min(from + size - 1, max));
+            searchToken = parameters.rangeBRCScheme.trapdoor(range);
+            final var ciphertexts =
+                    parameters.rangeBRCScheme.search(searchToken, parameters.encryptedIndex);
+            printer.printToCsv(
+                    "range",
+                    range.getMinimum(),
+                    range.getMaximum(),
+                    "token",
+                    searchToken.size(),
+                    "ciphertexts",
+                    ciphertexts.size(),
+                    constants);
+        }
+    }
+
+    @Benchmark
+    public Set<Ciphertext> search(
+            @NotNull Parameters rangeSchemeParameters, @NotNull RangeSchemeState state) {
+        return rangeSchemeParameters.rangeBRCScheme.search(
+                state.searchToken, rangeSchemeParameters.encryptedIndex);
+    }
+}
