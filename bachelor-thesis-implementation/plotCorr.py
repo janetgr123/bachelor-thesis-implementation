a07b9d802f7d60856353b7da6d7a866572356f1a
Janet Greutmann <janetgr@student.ethz.ch>
2024-02-16 19:56:15 +0100

    change corr plot

--- bachelor-thesis-implementation/plotCorr.py
+++ bachelor-thesis-implementation/plotCorr.py
@@ -3,7 +3,7 @@ import numpy as np
 
 x = np.linspace(0, 1, 100)
 x = x[10:] # remove 0
-y = 73.5 / x 
+y = 27.7 / x 
 plt.plot(x, y, color='black', linestyle='solid', linewidth=0.5)
 plt.ylabel('$\ell^*(\lambda)$')
 plt.xlabel('$\epsilon$')
