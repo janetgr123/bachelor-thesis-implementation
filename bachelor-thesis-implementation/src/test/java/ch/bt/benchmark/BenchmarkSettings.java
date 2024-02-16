6adedb83e402d79451cf3a83bea51a960876a511
Janet Greutmann <janetgr@student.ethz.ch>
2024-02-16 19:52:15 +0100

    fix settings

--- bachelor-thesis-implementation/src/test/java/ch/bt/benchmark/BenchmarkSettings.java
+++ bachelor-thesis-implementation/src/test/java/ch/bt/benchmark/BenchmarkSettings.java
@@ -9,7 +9,7 @@ public class BenchmarkSettings {
     public static final int NUMBER_OF_QUERIES =
             100; // number of queries for fixed parameters (e.g. data size, range size)
     public static final int WARM_UPS = 2; // number of warm-up iterations per benchmark run
-    public static final int MAX_NUMBER_OF_DATA_SAMPLES = 2097152; // 2^21
-    public static final int DOMAIN_SIZE = 2097152; // 2^21
+    public static final int MAX_NUMBER_OF_DATA_SAMPLES = 16384; // 2^14
+    public static final int DOMAIN_SIZE = 16384; // 2^14
     public static final int ITERATIONS_BUILD_INDEX = 5;
 }
