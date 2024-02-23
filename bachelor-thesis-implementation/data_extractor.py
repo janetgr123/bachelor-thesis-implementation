9e153b6ca3f2abf64523d0da9fdb2aeffb7f6d53
Janet Greutmann <janetgr@student.ethz.ch>
2024-02-23 17:24:20 +0100

    data for plots 2

--- bachelor-thesis-implementation/data_extractor.py
+++ bachelor-thesis-implementation/data_extractor.py
@@ -3,11 +3,13 @@ from os.path import exists
 
 PATH = "src/test/resources/data/" 
 SUBFOLDER = "dataForPlots/"
-indices = list(range(0,45,4))
+indices = list(range(0, 45, 4))
 indices.append(56)
 indices.append(60)
 indices.append(72)
 indices.append(76)
+indices.append(104)
+indices.append(108)
 
 # data size vs. time
 methods = ["buildIndex", "trapdoor", "search", "trapdoor2", "search2"]
@@ -19,45 +21,61 @@ for method in methods:
             df = df[df['emm'] != 'emm']
             data_sizes = df['data size'].unique()
             list = []
-            for data_size in data_sizes:
+            if method == 'buildIndex':
+                for data_size in data_sizes:
+                    rows = df.loc[df['data size'] == data_size]
+                    average = rows['time [ns]'].astype(float).mean(axis=0)
+                    list.append((data_size, average))
+            else:
+                data_size = data_sizes[-1]
                 rows = df.loc[df['data size'] == data_size]
-                average = rows['time [ns]'].astype(float).mean(axis=0)
-                list.append((data_size, average))
+                ranges = rows['range size'].unique()
+                for range_size in ranges:
+                    rows2 = rows.loc[rows['range size'] == range_size]
+                    average = rows2['time [ns]'].astype(float).mean(axis=0)
+                    list.append((range_size, average))
             new_df = pd.DataFrame(list)
             new_df.to_csv(PATH + SUBFOLDER + method + "-" + str(index) + "-dataSizeVsTime.csv", header=False, index=False)
 
 
-# encrypted index size (bytes) and percentage padding
-df = pd.read_csv(PATH + "overheadEncryptedIndex-" + str(index) + ".csv")
-df = df[df['emm'] != 'emm']
-data_sizes = df['data size'].unique()
-list = []
-list2 = []
-for data_size in data_sizes:
-    rows = df.loc[df['data size'] == data_size]
-    average = rows['size encrypted index'].astype(float).mean(axis=0)
-    list.append((data_size, average))
-    average2 = rows['number of dummy values'].astype(float).mean(axis=0)
-    list2.append((data_size, average2 / average * 100))
-new_df = pd.DataFrame(list)
-new_df.to_csv(PATH + SUBFOLDER + "dataSizeVsSizeIndexInBytes-" + str(index) + ".csv", header=False, index=False)
-new_df2 = pd.DataFrame(list2)
-new_df2.to_csv(PATH + SUBFOLDER + "dataSizeVsPercentagePaddingInBytes-" + str(index) + ".csv", header=False, index=False)
+        # encrypted index size (bytes) and percentage padding
+        df = pd.read_csv(PATH + "overheadEncryptedIndex-" + str(index) + ".csv")
+        df = df[df['emm'] != 'emm']
+        data_sizes = df['data size'].unique()
+        list = []
+        list2 = []
+        for data_size in data_sizes:
+            rows = df.loc[df['data size'] == data_size]
+            average = rows['size encrypted index'].astype(float).mean(axis=0)
+            list.append((data_size, average))
+            average2 = rows['number of dummy values'].astype(float).mean(axis=0)
+            list2.append((data_size, average2 / average * 100))
+        new_df = pd.DataFrame(list)
+        new_df.to_csv(PATH + SUBFOLDER + "dataSizeVsSizeIndexInBytes-" + str(index) + ".csv", header=False, index=False)
+        new_df2 = pd.DataFrame(list2)
+        new_df2.to_csv(PATH + SUBFOLDER + "dataSizeVsPercentagePaddingInBytes-" + str(index) + ".csv", header=False, index=False)
 
 
-# response size (entries) and percentage padding
-df = pd.read_csv(PATH + "searchPadding-" + str(index) + ".csv")
-df = df[df['emm'] != 'emm']
-data_sizes = df['data size'].unique()
-list = []
-list2 = []
-for data_size in data_sizes:
-    rows = df.loc[df['data size'] == data_size]
-    average = rows['size of response'].astype(float).mean(axis=0)
-    list.append((data_size, average))
-    average2 = rows['number of dummy values'].astype(float).mean(axis=0)
-    list2.append((data_size, average2 / average * 100))
-new_df = pd.DataFrame(list)
-new_df.to_csv(PATH + SUBFOLDER + "responseSize-" + str(index) + ".csv", header=False, index=False)
-new_df2 = pd.DataFrame(list2)
-new_df2.to_csv(PATH + SUBFOLDER + "responsePercentagePaddingInBytes-" + str(index) + ".csv", header=False, index=False)
+        # response size (entries) and percentage padding
+        df = pd.read_csv(PATH + "searchPadding-" + str(index) + ".csv")
+        df = df[df['emm'] != 'emm']
+        data_sizes = df['data size'].unique()
+        data_size = data_sizes[-1]
+        rows_data = df.loc[df['data size'] == data_size]
+        range_sizes = rows_data['from'].unique() # result printer headers are wrong!
+        list = []
+        list2 = []
+        for range_size in range_sizes:
+            rows = rows_data.loc[rows_data['from'] == range_size]
+            average = rows['range size'].astype(float).mean(axis=0)
+            list.append((range_size, average))
+            if average < 0.00001:
+                average2 = 0
+                list2.append((range_size, average2))
+            else:
+                average2 = rows['size of response'].astype(float).mean(axis=0)
+                list2.append((range_size, average2 / average * 100))
+        new_df = pd.DataFrame(list)
+        new_df.to_csv(PATH + SUBFOLDER + "responseSize-" + str(index) + ".csv", header=False, index=False)
+        new_df2 = pd.DataFrame(list2)
+        new_df2.to_csv(PATH + SUBFOLDER + "responsePercentagePaddingInEntries-" + str(index) + ".csv", header=False, index=False)
