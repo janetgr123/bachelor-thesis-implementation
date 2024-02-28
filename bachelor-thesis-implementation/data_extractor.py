2b1f5e8568c89ab32af32a9f781b9bb221e9e713
Janet Greutmann <janetgr@student.ethz.ch>
2024-02-28 17:57:39 +0100

    extracted data

--- bachelor-thesis-implementation/data_extractor.py
+++ bachelor-thesis-implementation/data_extractor.py
@@ -3,7 +3,7 @@ from os.path import exists
 
 PATH = "src/test/resources/data/" 
 SUBFOLDER = "dataForPlots/" 
-indices = [60]
+indices = [76]
 
 # data size vs. time
 methods = ["buildIndex", "trapdoor", "search", "trapdoor2", "search2"]
