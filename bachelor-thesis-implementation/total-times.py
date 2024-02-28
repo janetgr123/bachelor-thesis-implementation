93d9daf9264089f6b4c004906f88a0589b07bed1
Janet Greutmann <janetgr@student.ethz.ch>
2024-02-28 18:45:32 +0100

    extracted data again

--- bachelor-thesis-implementation/total-times.py
+++ bachelor-thesis-implementation/total-times.py
@@ -3,7 +3,7 @@ from os.path import exists
 
 PATH = "src/test/resources/data/" 
 SUBFOLDER = "dataForPlots/"
-indices = [76]
+indices = [88]
 #indices2 = list(range(48, 141, 4))
 #indices += indices2
 
