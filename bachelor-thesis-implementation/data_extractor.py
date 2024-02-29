51a589b6d9b2ce41f96e03de6c597b18aa2a80bc
Janet Greutmann <janetgr@student.ethz.ch>
2024-02-29 14:02:23 +0100

    extracted data

--- bachelor-thesis-implementation/data_extractor.py
+++ bachelor-thesis-implementation/data_extractor.py
@@ -3,7 +3,7 @@ from os.path import exists
 
 PATH = "src/test/resources/data/" 
 SUBFOLDER = "dataForPlots/" 
-indices = [180]
+indices = [6]
 
 # data size vs. time
 methods = ["buildIndex", "trapdoor", "search", "trapdoor2", "search2"]
--- bachelor-thesis-implementation/interactive-cumulation.py
+++ bachelor-thesis-implementation/interactive-cumulation.py
@@ -3,7 +3,7 @@ from os.path import exists
 
 PATH = "src/test/resources/data/" 
 SUBFOLDER = "dataForPlots/"
-indices = [1]
+indices = [168]
 
 # data size vs. time
 methods = ["trapdoor", "search"]
