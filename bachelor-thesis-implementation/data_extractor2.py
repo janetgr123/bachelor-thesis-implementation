4765fb64295e89f7109e862f0d25b68f704777e1
Janet Greutmann <janetgr@student.ethz.ch>
2024-03-19 12:04:25 +0100

    padding data

--- bachelor-thesis-implementation/data_extractor2.py
+++ bachelor-thesis-implementation/data_extractor2.py
@@ -3,8 +3,8 @@ from os.path import exists
 
 PATH = "src/test/resources/data2/" 
 SUBFOLDER = "dataForPlots/" 
-indices = [46,50]
-interactive = 1
+indices = [28]
+interactive = 0
 
 # response size (entries) and percentage padding
 for index in indices:
